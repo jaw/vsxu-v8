ee2a364399fa70ee96ac517f9c399c01c5220700
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2013-11-06 16:20:25 +0100

    Added v8

--- vsx_sample_module.h
+++ vsx_sample_module.h
@@ -20,10 +20,14 @@
 
 #ifndef VSX_SAMPLE_MODULE_H
 #define VSX_SAMPLE_MODULE_H
+#include <v8.h>
 
 #include "vsx_math_3d.h"
 #include "vsx_param.h"
 #include "vsx_module.h"
+#include "vsx_timer.h"
+
+using namespace v8;
 
 class vsx_sample_module : public vsx_module
 {
@@ -31,10 +35,111 @@ class vsx_sample_module : public vsx_module
   vsx_module_param_float* param2;
   vsx_module_param_float* result;
 
+  vsx_timer tt;
+  HandleScope handle_scope;
+  Handle<Script> script;
+  Handle<String> source;
+  //Persistent<Context> context;
+  Handle<Context> context;
+  Handle<Value> v8result;
+
 public:
-  virtual void module_info(vsx_module_info* info);
-  virtual void declare_params(vsx_module_param_list& in_parameters, vsx_module_param_list& out_parameters);
-  virtual void run();
+
+  void module_info(vsx_module_info* info)
+  {
+    info->identifier = "sample;module";
+    info->description = "Adds two input floats. And Returns an output float.";
+    info->in_param_spec ="param1:float,param2:float";;
+    info->out_param_spec = "result:float";
+    info->component_class = "parameters";
+  }
+
+  void declare_params(vsx_module_param_list& in_parameters, vsx_module_param_list& out_parameters)
+  {
+    param1 = (vsx_module_param_float*)in_parameters.create(VSX_MODULE_PARAM_ID_FLOAT,"param1");
+    param2 = (vsx_module_param_float*)in_parameters.create(VSX_MODULE_PARAM_ID_FLOAT,"param2");
+
+    result = (vsx_module_param_float*)out_parameters.create(VSX_MODULE_PARAM_ID_FLOAT,"result");
+
+    param1->set(0);
+    param2->set(0);
+    result->set(0);
+
+    loading_done = true;
+  }
+
+
+  bool init()
+  {
+    // Create a new context.
+    context = Context::New();
+
+
+    // Enter the created context for compiling and
+    // running the hello world script.
+    Context::Scope context_scope(context);
+
+    // Create a string containing the JavaScript source code.
+    source = String::New(
+      ""
+      "foo = 1.0; "
+      "function test_function()"
+      "{ "
+      "   foo+= 0.1;"
+      "   return foo;"
+      "}"
+    );
+    /*
+      "  var match = 0;"
+      "  if(arguments[0] == arguments[1])"
+      "   {"
+      "     match = 1;"
+      "   }"
+    */
+    // Compile the source code.
+    script = Script::Compile(source);
+
+    // Run the script to get the result.
+    v8result = script->Run();
+
+    //context.Dispose();
+    return true;
+  }
+
+
+
+  void run()
+  {
+    tt.start();
+    Context::Scope context_scope(context);
+
+    Handle<Object> global = context->Global();
+    Handle<Value> value = global->Get(String::New("test_function"));
+    Handle<Function> func = Handle<Function>::Cast(value);
+    Handle<Value> args[2];
+    Handle<Value> js_result;
+    int final_result;
+
+    args[0] = String::New("1");
+    args[1] = String::New("1");
+
+    js_result = func->Call(global, 2, args);
+    String::AsciiValue ascii(js_result);
+
+    Number* js_numresult = Number::Cast( *js_result );
+
+    final_result = atoi(*ascii);
+
+    //context.Dispose();
+
+    float t = tt.dtime();
+    printf("%d result %d run in %f seconds\n", this, final_result, t);
+    printf("     %f\n", js_numresult->Value());
+
+    //result->set(param1->get()+param2->get());
+  }
+
+
 };
 
 #endif // VSX_SAMPLE_MODULE_H
